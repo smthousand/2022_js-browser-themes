5409b387d984db31978d748a430101d4e0f7752a
smthousand <102133017+smthousand@users.noreply.github.com>
2022-06-19 05:12:01 +0900

    greeting finish

--- script/greeting.js
+++ script/greeting.js
@@ -1,10 +1,37 @@
+const greeting = document.querySelector(".main__greeting");
+const userInterface = document.querySelector(".main__user-interface"); 
+
 const loginForm = document.querySelector(".login__form");
-// const loginName = document.querySelector(".login__form #name");
-// const loginPassword = document.querySelector(".login__form #password");
-const loginSubmit = document.querySelector(".login__form input:last-child");
+const loginInput = document.querySelector(".login__form input");
+
+const HIDDEN_CLASSNAME = "hidden" 
+// string만 포함된 변수는 대문자로 표기하고 string을 저장하고 싶을 때 사용함
+// 중요한 정보가 아님
 
 function onLoginSubmit(event) {
-
+    event.preventDefault();
+    const valueResult = loginInput.value;
+    greeting.classList.add(HIDDEN_CLASSNAME);
+    userInterface.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginSubmit.addEventListener("submit", onLoginSubmit);
+loginForm.addEventListener("submit", onLoginSubmit);
+
+
+// const greeting = document.querySelector(".main__greeting"); 
+// const userInterface = document.querySelector(".main__user-interface"); 
+
+// const loginForm = document.querySelector(".login__form");
+// const loginInput = document.querySelector(".login__form input");
+
+// const HIDDEN_CLASSNAME = "hidden" 
+// // string만 포함된 변수는 대문자로 표기하고 string을 저장하고 싶을 때 사용함
+// // 중요한 정보가 아님
+
+// function onLoginSubmit(event) {
+//     event.preventDefault();
+//     const valueResult = loginPassword.value;
+//     greeting.classList.add(HIDDEN_CLASSNAME);
+// }
+
+// loginForm.addEventListener("submit", onLoginSubmit);
