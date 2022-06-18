ea94663314530d01548d5a195984b9e761f6fc66
smthousand <102133017+smthousand@users.noreply.github.com>
2022-06-19 02:11:05 +0900

    delete useless file

---
+++ script/greeting.js
@@ -0,0 +1,15 @@
+// const loginForm = document.querySelector(".login__form");
+// const loginName = document.querySelector(".login__form #name");
+// const loginPassword = document.querySelector(".login__form #password");
+// const loginSubmit = document.querySelector(".login__form input:last-child");
+
+// function onLoginBtnClick() {
+//     const name = loginName.value;
+//     if (name === "") {
+//         alert("Please write your name");
+//     } else if(name.length > 10) {
+//         alert("Your name is too long.");
+//     }
+// }
+
+// loginSubmit.addEventListener("click", onLoginBtnClick);
