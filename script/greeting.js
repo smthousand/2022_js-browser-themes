482408246c902beffc8ab7f5d92089c8ef989c45
smthousand <102133017+smthousand@users.noreply.github.com>
2022-06-19 02:34:53 +0900

    error

--- script/greeting.js
+++ script/greeting.js
@@ -1,15 +1,10 @@
-// const loginForm = document.querySelector(".login__form");
+const loginForm = document.querySelector(".login__form");
 // const loginName = document.querySelector(".login__form #name");
 // const loginPassword = document.querySelector(".login__form #password");
-// const loginSubmit = document.querySelector(".login__form input:last-child");
+const loginSubmit = document.querySelector(".login__form input:last-child");
 
-// function onLoginBtnClick() {
-//     const name = loginName.value;
-//     if (name === "") {
-//         alert("Please write your name");
-//     } else if(name.length > 10) {
-//         alert("Your name is too long.");
-//     }
-// }
+function onLoginSubmit(event) {
 
-// loginSubmit.addEventListener("click", onLoginBtnClick);
+}
+
+loginSubmit.addEventListener("submit", onLoginSubmit);
