227c8f7663301843f3786269ea68e14220e379c3
smthousand <102133017+smthousand@users.noreply.github.com>
2022-06-20 04:35:02 +0900

    add random images

--- script/greeting.js
+++ script/greeting.js
@@ -5,33 +5,24 @@ const loginForm = document.querySelector(".login__form");
 const loginInput = document.querySelector(".login__form input");
 
 const HIDDEN_CLASSNAME = "hidden" 
-// string만 포함된 변수는 대문자로 표기하고 string을 저장하고 싶을 때 사용함
-// 중요한 정보가 아님
 
 function onLoginSubmit(event) {
     event.preventDefault();
-    const valueResult = loginInput.value;
+    // const valueResult = loginInput.value;
     greeting.classList.add(HIDDEN_CLASSNAME);
     userInterface.classList.remove(HIDDEN_CLASSNAME);
 }
 
+
+const imagesArray = ["welcome.gif", "sign-visual.png", "sign-visual.gif"];
+const randomButton = document.querySelector(".login__random-btn");
+const loginImg = document.querySelector(".login__img");
+
+function changeImagesHandler() {
+    const mainVisual = imagesArray[Math.floor(Math.random() * imagesArray.length)];
+    loginImg.src = `images/${mainVisual}`;
+}
+
+
 loginForm.addEventListener("submit", onLoginSubmit);
-
-
-// const greeting = document.querySelector(".main__greeting"); 
-// const userInterface = document.querySelector(".main__user-interface"); 
-
-// const loginForm = document.querySelector(".login__form");
-// const loginInput = document.querySelector(".login__form input");
-
-// const HIDDEN_CLASSNAME = "hidden" 
-// // string만 포함된 변수는 대문자로 표기하고 string을 저장하고 싶을 때 사용함
-// // 중요한 정보가 아님
-
-// function onLoginSubmit(event) {
-//     event.preventDefault();
-//     const valueResult = loginPassword.value;
-//     greeting.classList.add(HIDDEN_CLASSNAME);
-// }
-
-// loginForm.addEventListener("submit", onLoginSubmit);
+randomButton.addEventListener("click", changeImagesHandler);
